978790d1c7fba3d6fd3605fc7c5677f3d36fee81
vladislav_nerubashchenko <blacktales911@yandex.ru>
2025-12-18 15:24:51 +0300

    refactor: change jwt expiration, secret destination

--- auth-service/src/main/java/com/example/auth_service/security/jwt/JwtService.java
+++ auth-service/src/main/java/com/example/auth_service/security/jwt/JwtService.java
@@ -3,6 +3,7 @@ package com.example.auth_service.security.jwt;
 import com.example.auth_service.entity.User;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
@@ -11,14 +12,19 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    // ВЫНЕСИ В application.yml позже
-    private static final String SECRET =
-            "super-secret-key-super-secret-key-super-secret-key";
+    private final String secret;
+    private final long expirationMs;
 
-    private static final long EXPIRATION_MS = 1000 * 60 * 60; // 1 час
+    public JwtService(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration-ms}") long expirationMs
+    ) {
+        this.secret = secret;
+        this.expirationMs = expirationMs;
+    }
 
     private Key getSigningKey() {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
+        return Keys.hmacShaKeyFor(secret.getBytes());
     }
 
     public String generateToken(User user) {
@@ -28,7 +34,7 @@ public class JwtService {
                 .claim("email", user.getEmail())
                 .claim("role", user.getRole().getName())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
