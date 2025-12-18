f2ed0e0c8f6bb877aa7e30f7e9b26215990f3433
vladislav_nerubashchenko <blacktales911@yandex.ru>
2025-12-18 17:05:09 +0300

    fix: request permissions in SecurityConfig

--- auth-service/src/main/java/com/example/auth_service/security/config/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/security/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/refresh", "/auth/logout").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session ->
